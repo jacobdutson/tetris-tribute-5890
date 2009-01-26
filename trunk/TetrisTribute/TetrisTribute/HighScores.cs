660a3b26e7aa71f83987e1c7a824ee77fd1bad69
newbyjn <newbyjn@93d05b32-ea58-11dd-a893-8d7ce9fbef62>
2009-01-26 08:08:47 +0000

    Added a XNA project

---
+++ trunk/TetrisTribute/TetrisTribute/HighScores.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TetrisTribute
+{
+    class HighScores
+    {
+        string[][] scores;
+        string filePath = "C://Users//Jason//Desktop//HighScores.txt";
+
+        public HighScores()
+        {
+          //  load or create a high score file
+            //get datad
+        }
+
+        public string[][] getScores()
+        {
+            //TODO if current score is a high score update the high scores
+
+            return scores;
+        }
+
+        public string[][] upDateScores(string[] currentScore){
+            //TODO if current score is a high score update the high scores
+
+            return scores;
+        }
+
+    }
+}
